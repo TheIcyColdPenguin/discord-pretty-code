1adc3aa64c36392240ab7116a891cb1f8e2ff0ed
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-04-12 02:33:26 +0530

    filtered empty strings from found code snippets

--- helpers.py
+++ helpers.py
@@ -6,7 +6,7 @@ CODE_BLOCK_CONTENT_FINDER = re.compile(r'```(\w*\n)?([^`]*)```')
 
 
 def get_code(string: str) -> List[str]:
-    return list(map(
+    return list(filter(lambda x: bool(x), map(
         lambda x: x[1].strip(),
         CODE_BLOCK_CONTENT_FINDER.findall(string)
-    ))
+    )))
