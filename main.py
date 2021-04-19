e82d381b8883888190932bbaebfd3f1d78a82adb
TheIcyColdPenguin <vinayakvineeth4@gmail.com>
2021-04-19 18:15:35 +0530

    switched to using BytesIO instead of saving to disk

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from io import BytesIO
+
 import discord
 
 from config import TOKEN
 from helpers import get_code
-from scraper import delete_img, save_img
+from scraper import get_img
 
 client = discord.Client()
 
@@ -30,9 +32,12 @@ async def on_message(msg: discord.Message):
     await msg.channel.send('Pretty code loading!')
 
     for code_snippet in code_snippets:
-        filename = save_img(code_snippet)
+        img_content = get_img(code_snippet)
+
         await msg.reply("Here's your code",
-                        file=discord.File(filename))
-        delete_img(filename)
+                        file=discord.File(
+                            fp=BytesIO(img_content),
+                            filename="code.png"
+                        ))
 
 client.run(TOKEN)
--- scraper.py
+++ scraper.py
@@ -1,6 +1,3 @@
-import os
-import random
-
 import requests
 
 from helpers import CodeObj, get_language_code
@@ -8,33 +5,11 @@ from helpers import CodeObj, get_language_code
 API_URL = 'https://carbonara.vercel.app/api/cook'
 
 
-def gen_possibly_used_filename():
-    return f'./images/tmp-image-\
-{str(random.randrange(1, 10000000000)).rjust(15, "0")}\
-.png'
-
-
-def gen_filename():
-    all_curr_files = os.listdir('./images')
-    while True:
-        maybe_filename = gen_possibly_used_filename()
-
-        if maybe_filename not in all_curr_files:
-            return maybe_filename
-
-
-def save_img(code: CodeObj):
+def get_img(code: CodeObj):
 
     response = requests.post(API_URL, json={
         'code': code['code'],
         "language": get_language_code(code['language']),
         "lineNumbers": True, })
-    filename = gen_filename()
-    with open(filename, 'wb') as new_img:
-        new_img.write(response.content)
 
-    return filename
-
-
-def delete_img(imagename: str):
-    os.remove(imagename)
+    return response.content
